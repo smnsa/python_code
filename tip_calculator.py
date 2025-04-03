184e36aa9de71d8fa2feba3623b5608313a7c948
smnsa <64846365+smnsa@users.noreply.github.com>
2025-04-04 07:16:10 +0900

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,5 +1,5 @@
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
+bill = float(input("What was the total bill? ¥"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 # チップの百分率
@@ -11,8 +11,8 @@ total_bill = bill + total_tip_amount
 # 一人当たりの支払い金額
 bill_per_person = total_bill / people
 # 一人当たりの金額を小数点以下２桁で丸める
-final_amount = round(bill_per_person, 2)
+final_amount = round(bill_per_person)
 # Fストリングスで表示
-print(f"Each person should pay: ${final_amount}")
+print(f"Each person should pay: ¥{final_amount}")
 
 
