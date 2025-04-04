73187de29d0a29557850a470a59e9790ed4784bf
smnsa <64846365+smnsa@users.noreply.github.com>
2025-04-04 09:08:20 +0900

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+color = input("What's your favorite color?\n")
+flower = input("What's the name of a favorite flower?\n")
+print("Your band name could be: " + color + " " + flower)
+
+
